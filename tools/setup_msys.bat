1523e94f3e9132c2a877ce4b85a5aaca6653a4da
github0null <me@github0null.io>
2023-03-23 10:11:29 +0800

    add missed dlls for win32 msys

--- tools/setup_msys.bat
+++ tools/setup_msys.bat
@@ -4,6 +4,7 @@ set CHECK_PATH=.\tools\msys\bin\bash.exe
 
 IF NOT EXIST %CHECK_PATH% (
     .\tools\7z\7za.exe x -y -r -aoa -o./tools ./tools/w800/toolchain/msys.7z
+    xcopy .\tools\msys-dlls\*.dll .\tools\msys\bin\ /H /Y
     echo  - setup msys done !
 ) else (
     echo  - found msys !, path: %CHECK_PATH%
